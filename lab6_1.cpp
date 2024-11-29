589cbf8744f2c5efc09861f2ad640764a8bea201
9nattch <nattchayadao@gmail.com>
2024-11-29 22:06:27 +0700

    "lab6_1.cpp completed"

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,24 @@
 using namespace std;
 
 int main(){
+    int number;
+    int even_number = 0;
+    int odd_number = 0;
+
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> number;
+
+    while(number != 0){
+        if(number % 2 == 0){
+            even_number++; 
+        }else{
+            odd_number++;
+        }
+        cout << "Enter an integer: ";
+        cin >> number;
+
+    }
+    cout << "#Even numbers = "<< even_number <<"\n";
+    cout << "#Odd numbers = " << odd_number <<"\n";
     return 0;
 }
